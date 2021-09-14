f0d9d8c3ccc285d47f8eb07b943cd6180e8d802b
Stephen Leisy <stephenleisy@Stephens-MacBook-Pro.local>
2021-09-14 11:30:50 -0700

    working on sizing changed 4

--- src/components/landing/LandingElements.js
+++ src/components/landing/LandingElements.js
@@ -18,7 +18,16 @@ export const TopContainer = styled.div`
 export const TopImage = styled.img`
   width: 100vw;
   height: 100%;
+  object-fit: cover;
   overflow-x: hidden;
+
+  @media screen and (max-width: 1030px) {
+    height: 160vmin;
+    width: 160vmin;
+    object-fit: center;
+    overflow: hidden;
+    font-size: 3rem;
+  }
 
   @media screen and (max-width: 780px) {
     height: 100vh;
