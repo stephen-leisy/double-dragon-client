f5ce6c7d4697192b969caeff1af4d7bfd013b52a
Stephen Leisy <stephenleisy@Stephens-MacBook-Pro.local>
2021-09-15 14:01:38 -0700

    started about/contact page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,74 @@
-import React from 'react';
+/* eslint-disable max-len */
+import React, { useEffect, useState } from 'react';
+
+import {
+  AboutLimits,
+  AboutWrapper,
+  AboutText,
+  Hours,
+  Address,
+  AddressHeader,
+  RickRoll,
+  MapLink,
+} from './AboutElements';
+import useScrollPosition from '@react-hook/window-scroll';
+import Header from '../header/Header';
+import Sidebar from '../mobileSidebar/MobileSidebar';
 
 export default function About() {
-  return <div>Sup</div>;
+  const [currentScroll, setCurrentScroll] = useState(0);
+  const [headerShow, setHeaderShow] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+  const scrollY = useScrollPosition(30 /*frames per second*/);
+
+  const scrollHeader = () => {
+    if (scrollY < 90) {
+      setHeaderShow(true);
+    } else if (scrollY < currentScroll) {
+      setHeaderShow(true);
+    } else {
+      setHeaderShow(false);
+    }
+  };
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    setCurrentScroll(scrollY);
+    scrollHeader();
+    console.log(isOpen);
+  }, [scrollY]);
+  
+  return (
+    <AboutLimits>
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Header
+        headerShow={headerShow}
+        scrollHeader={scrollHeader}
+        toggle={toggle}
+      />
+      <AboutWrapper>
+        <AboutText>
+          <Hours>OPEN:</Hours>
+          <Hours>SUNDAY-THURSDAY</Hours>
+          <Hours>12p-12a</Hours>
+          <Hours>FRIDAY & SATURDAY</Hours>
+          <Hours>12p-1a</Hours>
+          <AddressHeader>LOCATED AT:</AddressHeader>
+          <Address>1235 SE Division St, Portland, OR 97202</Address>
+
+          <MapLink
+            as="a"
+            href="https://goo.gl/maps/o8j7yuTs5RosrBrT8"
+            target="_blank"
+          >
+            MAP IT!
+          </MapLink>
+        </AboutText>
+        <RickRoll src="https://www.youtube.com/embed/cvh0nX08nRw?autoplay=1"></RickRoll>
+      </AboutWrapper>
+    </AboutLimits>
+  );
 }
---
+++ src/components/about/AboutElements.jsx
@@ -0,0 +1,83 @@
+import styled from 'styled-components';
+
+export const AboutLimits = styled.main`
+  height: 140vh;
+  width: 100%;
+  background: #f28d48;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const AboutWrapper = styled.div`
+  height: 80%;
+  width: 80%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding-top: 25vh;
+
+  @media screen and (max-width: 800px) {
+    width: 100%;
+    padding-top: 7vh;
+  }
+`;
+
+export const AboutText = styled.div`
+  border: 15px solid #dcad37;
+  height: 60%;
+  width: 60%;
+  font-family: 'Corben', cursive;
+  color: white;
+  font-size: 1.3rem;
+  -webkit-text-stroke: 2px black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: #f8f0e3;
+
+  @media screen and (max-width: 800px) {
+    width: 90%;
+    font-size: 0.7rem;
+    color: #f28d48;
+    -webkit-text-stroke: 0.5px black;
+  }
+`;
+
+export const Hours = styled.p``;
+
+export const AddressHeader = styled.p`
+  padding-top: 20px;
+`;
+
+export const Address = styled.p``;
+
+export const RickRoll = styled.iframe`
+  height: 60%;
+  width: 60%;
+  margin-top: 20px;
+  border: 15px solid #dcad37;
+
+  @media screen and (max-width: 800px) {
+    width: 80%;
+  }
+`;
+
+export const MapLink = styled.button`
+  padding-top: 10px;
+  font-size: 1.6rem;
+  color: #eca481;
+  text-decoration: none;
+  -webkit-text-stroke: 2px black;
+
+  &.active {
+    transform: scale(0.9);
+  }
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    transform: scale(1.1);
+    -webkit-text-stroke: 2.3px black;
+  }
+`;
